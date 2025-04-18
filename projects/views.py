40292d46585c23631beb7c263b1823d1ce1e2773
chrisdiazaguilar <chrisdiaz0641@gmail.com>
2025-04-17 20:38:33 -0400

    Worked on SQLite adding entries

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from projects.models import Project
 
 # Create your views here.
 
 
-def project_list(request):
-    return render(request, 'projects/index.html')
+def all_projects(request):
+    # query the db to return all project objects
+    projects = Project.objects.all()
+    return render(request, 'all_projects.html')
+    print(projects)
+    return render(request, 'all_projects.html', {'projects': projects})
